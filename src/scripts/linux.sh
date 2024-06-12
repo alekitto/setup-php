8e519b7267831af4181aea457e9850d52abafe15
Alessandro Chitolina <alekitto@gmail.com>
2024-06-12 17:41:05 +0200

    fix: do not use cached versions

--- src/scripts/linux.sh
+++ src/scripts/linux.sh
@@ -185,18 +185,12 @@ update_php() {
 
 # Function to install PHP.
 add_php() {
-  if [ "${runner:?}" = "self-hosted" ] || [ "${use_package_cache:-true}" = "false" ]; then
-    if [[ "$version" =~ ${nightly_versions:?} || "$ts" = "zts" ]]; then
-        setup_php_builder
-    else
-      add_packaged_php
-      switch_version >/dev/null 2>&1
-      add_pecl
-    fi
-  elif [[ "$version" =~ ${old_versions:?} ]]; then
-    setup_old_versions
+  if [[ "$version" =~ ${nightly_versions:?} || "$ts" = "zts" ]]; then
+    setup_php_builder
   else
-    setup_cached_versions
+    add_packaged_php
+    switch_version >/dev/null 2>&1
+    add_pecl
   fi
   status="Installed"
 }
